4c2e52aff326db385b731615b3b2ca3ba605b3da
James <j.wallen.jones@googlemail.com>
2016-02-03 00:32:37 +0000

    fix gulp maybe hopefully

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('styles', function(){
+    gulp.src(['resources/assets/**/*.scss'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }}))
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('public/css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css/'))
+});
+
+
+gulp.task('default', function(){
+    gulp.watch("resources/assets/**/*.scss", ['styles']);
 });
